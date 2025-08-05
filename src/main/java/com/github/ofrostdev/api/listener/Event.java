e7a6183153ef6f06ed1b54e82a40f62d79376b57
palhacin <palhacinpessoal@gmail.com>
2025-08-05 18:15:13 -0300

    Fix multiple events callback error

--- src/main/java/com/github/ofrostdev/api/listener/Event.java
+++ src/main/java/com/github/ofrostdev/api/listener/Event.java
@@ -13,7 +13,8 @@ import java.util.function.Consumer;
 public final class Event {
 
     private static Plugin plugin;
-    private static final Map<UUID, Consumer<org.bukkit.event.Event>> playerCallbacks = new ConcurrentHashMap<>();
+
+    private static final Map<UUID, Map<Class<? extends org.bukkit.event.Event>, Consumer<? extends org.bukkit.event.Event>>> playerCallbacks = new ConcurrentHashMap<>();
     private static final Map<Class<? extends org.bukkit.event.Event>, Consumer<? extends org.bukkit.event.Event>> globalCallbacks = new ConcurrentHashMap<>();
 
     private Event() {}
@@ -41,12 +42,15 @@ public final class Event {
                 plugin
         );
     }
+
     @SuppressWarnings("unchecked")
-    public static <E extends org.bukkit.event.Event> void listen(Class<E> eventClass, Player player, Consumer<E> callback) {
+    public static <E extends org.bukkit.event.Event> void listen(Class<E> eventClass, Player player, Consumer<E> callback, boolean keepListener) {
         if (plugin == null) throw new IllegalStateException("[FrostAPI] Event -> Registre com.github.ofrostdev.api.FrostAPI.enable(Plugin plugin) na main!");
 
         UUID uuid = player.getUniqueId();
-        playerCallbacks.put(uuid, (Consumer<org.bukkit.event.Event>) callback);
+
+        playerCallbacks.computeIfAbsent(uuid, k -> new ConcurrentHashMap<>())
+                .put(eventClass, callback);
 
         Bukkit.getPluginManager().registerEvent(
                 eventClass,
@@ -54,14 +58,28 @@ public final class Event {
                 org.bukkit.event.EventPriority.NORMAL,
                 (listener, event) -> {
                     if (eventClass.isInstance(event)) {
-                        Consumer<org.bukkit.event.Event> action = playerCallbacks.get(uuid);
-                        if (action != null) {
-                            action.accept(event);
+                        Map<Class<? extends org.bukkit.event.Event>, Consumer<? extends org.bukkit.event.Event>> map = playerCallbacks.get(uuid);
+                        if (map != null) {
+                            Consumer<E> action = (Consumer<E>) map.get(eventClass);
+                            if (action != null) {
+                                action.accept((E) event);
+
+                                if (!keepListener) {
+                                    map.remove(eventClass);
+                                    if (map.isEmpty()) {
+                                        playerCallbacks.remove(uuid);
+                                    }
+                                }
+                            }
                         }
                     }
                 },
                 plugin
         );
+    }
+
+    public static <E extends org.bukkit.event.Event> void listen(Class<E> eventClass, Player player, Consumer<E> callback) {
+        listen(eventClass, player, callback, false);
     }
 
     public static void cancel(Player player) {
