e64127403c1d3e47aeaee1348a014d293a9c0b3e
palhacin <palhacinpessoal@gmail.com>
2025-08-05 16:19:55 -0300

    Update FrostAPI.java

--- src/main/java/com/github/ofrostdev/api/FrostAPI.java
+++ src/main/java/com/github/ofrostdev/api/FrostAPI.java
@@ -5,7 +5,7 @@ import com.github.ofrostdev.api.manager.FrostManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FrostAPI {
-    public static void enabsle(JavaPlugin plugin) {
+    public static void enable(JavaPlugin plugin) {
         FrostManager.init(plugin);
         Event.init(plugin);
     }
