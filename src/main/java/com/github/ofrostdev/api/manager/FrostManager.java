37fbe21f9ef04c8ada1953aed1b2c10448723fe3
palhacin <palhacinpessoal@gmail.com>
2025-08-11 16:44:54 -0300

    Update FrostManager.java

--- src/main/java/com/github/ofrostdev/api/manager/FrostManager.java
+++ src/main/java/com/github/ofrostdev/api/manager/FrostManager.java
@@ -1,5 +1,6 @@
 package com.github.ofrostdev.api.manager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 public class FrostManager {
@@ -10,4 +11,26 @@ public class FrostManager {
         if (FrostManager.plugin != null) return;
         FrostManager.plugin = plugin;
     }
+
+    public static boolean hasPlugin(Plugin plugin){
+        if (plugin == null) return false;
+        Plugin found = Bukkit.getPluginManager().getPlugin(plugin.getName());
+        return found != null && found.isEnabled();
+    }
+
+    public static boolean hasPlugin(String name){
+        if (name == null || name.isEmpty()) return false;
+        Plugin plugin = Bukkit.getPluginManager().getPlugin(name);
+        return plugin != null && plugin.isEnabled();
+    }
+
+    public static boolean hasPluginClass(String clazz){
+        try {
+            Class.forName("br.com.ystoreplugins.product.ypesca.event.PescaFishEvent");
+            return true;
+        } catch (ClassNotFoundException ignored) {
+            return false;
+        }
+    }
+
 }
